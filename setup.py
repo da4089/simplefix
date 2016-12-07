156451b403613ed0de1fda1fcdc7638d767cd327
David Arnold <d@0x1.org>
2016-12-07 23:10:17 +1100

    Add skeleton files, sufficient to claim on PyPI.

---
+++ setup.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+########################################################################
+# Copyright (C) 2016, David Arnold.
+#
+#
+
+from distutils.core import setup
+
+
+setup(name="simplefix",
+      version="1.0.0",
+      description="Simple FIX Protocol implementation for Python",
+      url="https://github.com/da4089/simplefix",
+      author="David Arnold",
+      author_email="d+simplefix@0x1.org",
+      packages=["simplefix"])
+
+
+########################################################################
