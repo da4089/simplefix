f1d417d2866fb13f339795c6d7c25991ce1c604a
David Arnold <d@0x1.org>
2018-01-23 22:10:34 +1100

    Update copyright claim years.

--- test/test_constants.py
+++ test/test_constants.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 ########################################################################
 # SimpleFIX
-# Copyright (C) 2017, David Arnold.
+# Copyright (C) 2017-2018, David Arnold.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
