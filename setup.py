fbfb2852948d5a97ddaea69ae631ffd1af847312
David Arnold <d@0x1.org>
2018-09-28 16:01:30 +1000

    Note support for 3.7 in package metadata.

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(name="simplefix",
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         ],
       )
 
