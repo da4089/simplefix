da8e5e2b26526482415aea9ffc7dda0c14859bcd
David Arnold <d@0x1.org>
2022-10-26 21:10:39 +0000

    Add support for Python 3.11 release.

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setup(name="simplefix",
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         ],
       )
 
